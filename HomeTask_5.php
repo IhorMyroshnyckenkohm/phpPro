125469941f71e8ddc5ae5f4a23b1b287785b243d
Ihor Myroshnychenko <ihor.myroshnychenko@redoo-ukraine.com.ua>
2023-11-28 15:25:33 +0200

    рефакторинг класу

---
+++ HomeTask_5.php
@@ -0,0 +1,15 @@
+<?php
+class ProductData {
+    public function get($name) {}
+    public function set($name, $value) {}
+    public function save() {}
+    public function update() {}
+    public function delete() {}
+}
+class ProductProcessor {
+    public function process() {}
+}
+class ProductOutput {
+    public function show() {}
+    public function print() {}
+}
