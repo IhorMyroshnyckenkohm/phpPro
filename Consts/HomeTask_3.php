230c369d049b60544f992d48e971f29b10959c3f
Ihor Myroshnychenko <ihor.myroshnychenko@redoo-ukraine.com.ua>
2023-11-28 15:20:47 +0200

    3 трейти з назвами Trait1, Trait2 та Trait3.

---
+++ Consts/HomeTask_3.php
@@ -0,0 +1,42 @@
+<?php
+trait Trait1
+{
+    public function test()
+    {
+        return 1;
+    }
+}
+
+trait Trait2
+{
+    public function test()
+    {
+        return 2;
+    }
+}
+
+trait Trait3
+{
+    public function test()
+    {
+        return 3;
+    }
+}
+
+class Test
+{
+    use Trait1, Trait2, Trait3 {
+        Trait1::test insteadof Trait2, Trait3;
+        Trait2::test as testTrait2;
+        Trait3::test as testTrait3;
+    }
+
+    public function getSum()
+    {
+        return $this->test() + $this->testTrait2() + $this->testTrait3();
+    }
+}
+
+// Використання
+$testObject = new Test();
+echo $testObject->getSum(); // Результат буде 6
