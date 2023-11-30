8d2956390129c08528bd0a61ab8c0f1732558ea4
Ihor Myroshnychenko <ihor.myroshnychenko@redoo-ukraine.com.ua>
2023-11-30 17:12:32 +0200

    виправлення помилок стосовно метода __call

--- HomeWork_4.php
+++ HomeWork_4.php
@@ -1,9 +1,10 @@
 <?php
+
 class User
 {
-    private $name;
-    private $age;
-    private $email;
+    public $name;
+    public $age;
+    public $email;
 
     public function __construct($name, $age, $email)
     {
@@ -12,22 +13,22 @@ class User
         $this->setEmail($email);
     }
 
-    private function setName($name)
+    public function setName($name)
     {
         $this->name = $name;
     }
 
-    private function setAge($age)
+    public function setAge($age)
     {
         $this->age = $age;
     }
 
-    private function setEmail($email)
+    public function setEmail($email)
     {
         $this->email = $email;
     }
 
-    public function getAll()
+    public function getAll(): array
     {
         return [
             'name' => $this->name,
@@ -36,21 +37,17 @@ class User
         ];
     }
 
+    /**
+     * @throws CustomException
+     */
     public function __call($method, $arguments)
     {
-        $prefix = substr($method, 0, 3);
         $propertyName = lcfirst(substr($method, 3));
 
-        switch ($prefix) {
-            case 'set':
-                if (property_exists($this, $propertyName)) {
-                    $this->$propertyName = $arguments[0];
-                } else {
-                    throw new CustomException("Method {$method} does not exist.");
-                }
-                break;
-            default:
-                throw new CustomException("Method {$method} does not exist.");
+        if (method_exists($this, $method) && property_exists($this, $propertyName)) {
+            $this->$propertyName = $arguments[0];
+        } else {
+            throw new CustomException("Method {$method} does not exist.");
         }
     }
 }
@@ -59,14 +56,10 @@ class CustomException extends Exception
 {
 }
 
-// Використання
 try {
     $user = new User('John Doe', 25, 'john@example.com');
     $user->setAge(30);
     $user->setEmail('newemail@example.com');
-
-    // Невизначений метод, генерує CustomException
-    // $user->setUsername('johndoe');
 
     $userData = $user->getAll();
     print_r($userData);
